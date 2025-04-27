e86060a3cea2c4cbc3e0e73f4a317d818da16b13
Nigora880 <nx165880@gmail.com>
2025-04-27 16:09:10 +0300

    update commit

--- main.js
+++ main.js
@@ -1,18 +1,29 @@
-var a = prompt("1-son kiriting")-0
-var action = prompt ("action kiriting")
-var b =prompt("2-son kiriting")-0
+// var a = prompt("1-son kiriting")-0
+// var action = prompt ("action kiriting")
+// var b =prompt("2-son kiriting")-0
 
-switch(action){
-    case "+":
-        console.log(a+b);
-        break;
-case "*":
-    console.log(a*b);
-    break;
-case "-":
-        console.log(a-b);
-    break;
-case "/":
-        console.log(a/b);
-    break;
-}
+// switch(action){
+//     case "+":
+//         console.log(a+b);
+//         break;
+// case "*":
+//     console.log(a*b);
+//     break;
+// case "-":
+//         console.log(a-b);
+//     break;
+// case "/":
+//         console.log(a/b);
+//     break;
+// }
+
+//task 2
+//prompt ixtiyoriy son kiriting shu kiritilgan songa 1ni qoshib consolega chiqaring
+// var a = prompt("Iltimos sonni kiriting");
+// a++
+// console.log(a)
+
+var a = prompt("Iltimos sonni kiriting");
+if (a >=0 )
+console.log(Math.abs.apply(a) && "bu juft son")
+else ("bu son manfiy")

b77793ae3ea07dee80ffc65a7b053f272775680f
Nigora880 <nx165880@gmail.com>
2025-04-27 15:32:14 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var a = prompt("1-son kiriting")-0
+var action = prompt ("action kiriting")
+var b =prompt("2-son kiriting")-0
+
+switch(action){
+    case "+":
+        console.log(a+b);
+        break;
+case "*":
+    console.log(a*b);
+    break;
+case "-":
+        console.log(a-b);
+    break;
+case "/":
+        console.log(a/b);
+    break;
+}
